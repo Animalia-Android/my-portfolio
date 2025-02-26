772d961e1fa06fa3f438401b55ef8afae960de23
Doug Sellers <DouglasSSellers@gmail.com>
2025-02-25 19:09:25 -0500

    Added correct image tile for Vid Vault

--- src/ProjectsCarousel.jsx
+++ src/ProjectsCarousel.jsx
@@ -58,7 +58,7 @@ const projects = [
     github: 'https://github.com/Animalia-Android/Video-Player',
     live: 'https://video-player-with-hooks.vercel.app/',
     image:
-      'https://plus.unsplash.com/premium_photo-1663126351065-741a1d338b5d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8UmVjaXBlc3xlbnwwfDB8MHx8fDA%3D',
+      'https://images.unsplash.com/photo-1611162616475-46b635cb6868?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8VmlkZW8lMjBWYXVsdHxlbnwwfDB8MHx8fDI%3D',
   },
 ];
 
@@ -100,24 +100,27 @@ const ProjectCard = styled.div`
 
 const Image = styled.img`
   width: 100%;
+  height: 40%;
   border-radius: 8px;
 `;
 
 const ProjectTitle = styled.h3`
   font-size: 1.5rem;
   color: ${(props) => props.theme.text};
-  margin-top: 10px;
+  margin-top: 20px;
 `;
 
 const Description = styled.p`
   color: #b0bec5;
   font-size: 1rem;
+  height: 15%;
   margin: 10px 0;
 `;
 
 const TechStack = styled.p`
   font-size: 0.9rem;
   color: #00c6ff;
+  height: 10%;
   margin-bottom: 10px;
 `;
 
