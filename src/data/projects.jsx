f4f55c0ec2475e7d426ca3c51e1beb9d4f27c01a
Doug Sellers <DouglasSSellers@gmail.com>
2025-09-14 14:02:58 -0400

    added new project remote dev

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -61,6 +61,16 @@ const projects = [
     image:
       'https://images.unsplash.com/photo-1613210434051-4b00d62d03fb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHN0YWdlfGVufDB8fDB8fHwy',
   },
+  {
+    title: 'Remote Dev',
+    description: 'Search for new Dev jobs.',
+    tech: 'React, Node.js, API',
+    github: 'https://github.com/Animalia-Android/remote-job-finder',
+    live: 'https://remote-job-finder-chi.vercel.app/',
+    featured: true,
+    image:
+      'https://images.unsplash.com/photo-1530977875151-aae9742fde19?q=80&w=2679&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
 ];
 
 // Add more projects...
